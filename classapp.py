6c976e161288ea3b5d1764894829f8d5302c1041
Satvick Beldhari <129999055+satvickbeldhari@users.noreply.github.com>
2025-05-25 23:26:11 +0530

    Update classapp.py

--- classapp.py
+++ classapp.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pickle
 import numpy as np
+import pandas as pd
 
 # Set page configuration
 st.set_page_config(page_title="Health Classification App", page_icon="🏋️", layout="centered")
